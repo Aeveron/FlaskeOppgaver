d0adcdcfd37ea20f6c022a005d28e91cc086822a
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-10-11 21:05:01 +0200

    x
    x

---
+++ TestBottle.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Xml.Schema;
+
+namespace FlaskeOppgaver
+{
+    public class TestBottle
+    {
+        public int Capacity { get; }
+        public int Content { get; private set; }
+
+        public TestBottle(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public void FillToTopFromTap()
+        {
+            Content = Capacity;
+        }
+
+        public void Fill(int volume)
+        {
+            Content = Math.Min(Content + volume, Capacity);
+        }
+
+        public int FillToTop(TestBottle bottle1, TestBottle bottle2)
+        {
+            var volumeFilled = Capacity - Content;
+            Content = Capacity;
+            return volumeFilled;
+
+        }
+
+        public int Empty()
+        {
+            var content = Content;
+            Content = 0;
+            return content;
+        }
+    }
+}
